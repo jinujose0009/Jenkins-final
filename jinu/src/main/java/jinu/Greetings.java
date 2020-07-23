10ff7a90e3ad33cd93253610ab77298d787bb9fc
jinu <jinujose0009@gmail.com>
2020-07-23 08:19:37 +0530

    First commit

---
+++ jinu/src/main/java/jinu/Greetings.java
@@ -0,0 +1,11 @@
+package jinu;
+
+public class Greetings {
+
+	public String gteet(String msg) {
+		// TODO Auto-generated method stub
+		return "Hellow" +msg;
+	}
+
+	
+}
---
+++ jinu/src/main/java/jinu/Helloworld.java
@@ -0,0 +1,11 @@
+package jinu;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greetings obj=new Greetings();
+		System.out.println(obj.gteet("Jenkins"));
+	}
+
+}
